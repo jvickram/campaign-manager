fa8616a49967ee8845c4428f51d66f2ee4502b7d
Bickey Jaisawal <jvickram2@gmail.com>
2020-12-24 22:25:12 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1>Hello! Welcome to React!!!!!!</h1>      
+          <h1>Campaign Manager</h1>      
         </div>
     );
   }
